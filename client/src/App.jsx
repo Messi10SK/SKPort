ada59eb5feb8a05503dd2b1b9336e69f257a9772
Satyam Kanojiya <kanojiyasatyam07@gmail.com>
2024-04-22 21:26:35 +0530

    added pages and navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+
+
+function App() {
+
+
+  return (
+   <BrowserRouter>
+   
+   <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/about" element={<About/>} />
+    <Route path="/contact" element={<Contact/>} />
+    <Route path="/projects" element={<Projects/>} />
+
+
+   </Routes>
+   
+   </BrowserRouter>
+  )
+}
+
+export default App
