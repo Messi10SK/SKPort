2377188bc93676b19e2d754207ca0fd189d4837e
Satyam Kanojiya <kanojiyasatyam07@gmail.com>
2024-05-27 20:37:49 +0530

    another update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './pages/About'
 import Contact from './pages/Contact'
-import Projects from './pages/Projects'
 
 
 function App() {
@@ -18,9 +16,9 @@ function App() {
    
    <Routes>
     <Route path="/" element={<Home/>} />
-    <Route path="/about" element={<About/>} />
+  
     <Route path="/contact" element={<Contact/>} />
-    <Route path="/projects" element={<Projects/>} />
+  
 
 
    </Routes>
